9a28468a576bf69f535a60a75647ac8b65517e70
tuongprolaai <phuctuong123456@gmail.com>
2026-05-03 16:03:17 +0700

    Done Brand Box

--- js/custom.js
+++ js/custom.js
@@ -58,4 +58,23 @@ $(document).ready(function () {
             },
         },
     });
+
+    // Brand on slider
+    $(".brand-box__list").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 5,
+            },
+        },
+    });
 });
