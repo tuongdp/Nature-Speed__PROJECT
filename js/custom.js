9d499554a33ba98de8a06935dee01a92deda221a
tuongdo <tuongdp@imt-soft.com>
2026-05-05 08:18:30 +0700

    0818AM05052026

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   });
 
   // Product box slider
-  $(".product-box--lists").owlCarousel({
+  $(".product-home .product-box--lists").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
